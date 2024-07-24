55023e7ead5abfe23d851d2acd83f58381aae9a2
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-07-24 09:02:13 +0100

    task1 done

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,10 +7,8 @@
 import sys
 import urllib.request
 
-
-url = sys.argv[1]
-
 if __name__ == "__main__":
+    url = sys.argv[1]
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
