4d277c3b5b43bef6b23a417483b5da8455061df2
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-07-24 08:54:54 +0100

    reviewed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,6 +11,7 @@ import urllib.request
 url = sys.argv[1]
 
 if __name__ == "__main__":
+
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
